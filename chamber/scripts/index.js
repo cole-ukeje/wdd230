760d7afb7b884cad8d66c1dfffbbf39bf0fdaa6e
iheanyi11 <ejeagbaakuegbo@gmail.com>
2025-03-31 13:38:16 +0100

    Join page done. Thankyou page done. Added links to the nav of all page

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -177,3 +177,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Update time every second
     setInterval(updateDateTime, 1000);
 });
+
+// Timestamp value
+window.onload = function () {
+    document.getElementById("timestamp").value = new Date().toLocaleString();
+};
+
+// Open Modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Close Modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside
+window.onclick = function (event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
